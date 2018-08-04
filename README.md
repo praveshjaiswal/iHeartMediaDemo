18ccc22feaebf7b8af97608ae7468b4c325e9150
Jaiswal <praveshjaiswal@gmail.com>
2018-08-03 23:53:43 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# iHeartMediaDemo" 
+"# iHeartMediaDemo" 
---
+++ src/main/java/com/java/iHMDemo/IHmDemoApplication.java
@@ -0,0 +1,12 @@
+package com.java.iHMDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHmDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IHmDemoApplication.class, args);
+	}
+}
